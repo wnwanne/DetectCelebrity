b9b831f9ee951a29665a985bfe9916271d52f299
Winston Nwanne <wnwanne@gmail.com>
2020-09-09 16:30:32 -0400

    improved functionality

--- app/forms.py
+++ app/forms.py
@@ -10,7 +10,6 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
-
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
--- app/routes.py
+++ app/routes.py
@@ -4,24 +4,43 @@ from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user,login_required
 from app.models import User
 from werkzeug.urls import url_parse
+import boto3
 
 
-@app.route('/')
-@app.route('/index')
-@login_required
+bucket_name = "winnie-analyzevid-ab2"
+
+s3 = boto3.client("s3")
+
+@app.route("/", methods=['post', 'get'])
+@app.route("/index", methods=['post', 'get'])
 def index():
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', posts=posts)
+    if request.method == "POST":
+        try:
+            img = request.files['img']
+            filename = img.filename
+            if img:
+                s3.put_object(
+                    Bucket = bucket_name,
+                    Body = img,
+                    Key=filename
+                )
+                return("<h1>upload successful<h1>")
+        except Exception as e:
+            return (str(e))
+    return render_template("index.html")
 
+    # return '''<form method=POST enctype=multipart/form-data action="upload">
+    #     <input type=file name=myfile>
+    #     <input type=submit>
+    #     </form>'''
+
+# @app.route('/upload', methods=['POST'])
+# def upload():
+#     s3 = boto3.resource('s3')
+#
+#     s3.Bucket('winnie-analyzevid-ab2').put_object(Key='colortracingshot.mov', Body=request.files['myfile'])
+#
+#     return '<h1>File saved to S3</h1>'
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
